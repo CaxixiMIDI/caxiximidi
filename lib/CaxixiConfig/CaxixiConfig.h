966c01b8ffdac10829f7ac53277535c7982c2777
gomartinez <gomartinez@despegar.com>
2016-03-30 13:33:08 -0300

    Added octavator sequencer and led indication to receiver

--- lib/CaxixiConfig/CaxixiConfig.h
+++ lib/CaxixiConfig/CaxixiConfig.h
@@ -5,14 +5,30 @@ const int NOTE_BACKWARD = 1;
 const int NOTE_HIT = 2;
 const int NOTE_OFF = 5;
 
+// OCTAVE
+#define OCTAVE_DOWN_BUTTON_PIN 7
+#define OCTAVE_UP_BUTTON_PIN 6
+#define OCTAVE_LED_PIN 13
+#define OCTAVE_UP_LED_RED_PIN 10
+#define OCTAVE_UP_LED_BLUE_PIN 12
+#define OCTAVE_UP_LED_GREEN_PIN 11
+#define OCTAVE_DOWN_LED_RED_PIN 4
+#define OCTAVE_DOWN_LED_BLUE_PIN 10
+#define OCTAVE_DOWN_LED_GREEN_PIN 5
+
+// SAMPLER
+#define SAMPLER_BUTTON_RECORD_PIN 9
+#define SAMPLER_BUTTON_UNDO_PIN 8
+#define SAMPLER_BUFFER_SIZE 100
+
 // SENSOR_BEGIN
 const int SENSOR_ACCEL_X = 0;
 const int SENSOR_ACCEL_Y = 1;
 const int SENSOR_ACCEL_Z = 2;
 
 // SmoothFilter Samples
-const int filterSamples = 5;
-const int BUFFER_SIZE = 8;
+#define filterSamples 5
+#define BUFFER_SIZE 8
 
 const int SLOPE_STILL_X_RANGE = 20;
 const int SLOPE_STILL_Y_RANGE = 50;
@@ -33,12 +49,12 @@ const int STATE_BACKWARD = 2;
 
 const int RESOLUTION = 1;
 
-const int CAXIXI_RIGHT_NOTE_FORWARD = 43;
-const int CAXIXI_RIGHT_NOTE_BACKWARD = 45;
-const int CAXIXI_RIGHT_NOTE_HIT = 47;
-const int CAXIXI_LEFT_NOTE_FORWARD = 36;
-const int CAXIXI_LEFT_NOTE_BACKWARD = 38;
-const int CAXIXI_LEFT_NOTE_HIT = 40;
+#define CAXIXI_RIGHT_NOTE_FORWARD 43
+#define CAXIXI_RIGHT_NOTE_BACKWARD 45
+#define CAXIXI_RIGHT_NOTE_HIT 47
+#define CAXIXI_LEFT_NOTE_FORWARD 36
+#define CAXIXI_LEFT_NOTE_BACKWARD 38
+#define CAXIXI_LEFT_NOTE_HIT 40
 
 const int NOTE_RELEASE_FORWARD = 100;
 const int NOTE_RELEASE_BACKWARD = -100;
@@ -58,3 +74,12 @@ const int CAXIXI_LEFT_BACKWARD_NOTEON = 221;
 const int CAXIXI_LEFT_BACKWARD_NOTEOFF = 220;
 const int CAXIXI_LEFT_HIT_NOTEON = 231;
 const int CAXIXI_LEFT_HIT_NOTEOFF = 230;
+
+struct Buffer_s {
+  int note;
+  int layer;  //capa de sobregrabación. cada vez que se prende "record", suma 1.
+  bool encendido;// on/off
+  long time;
+} Buffer_default = {0,0,0,9999999};
+
+typedef struct Buffer_s Buffer;
--- src/receiver/receiver.ino
+++ src/receiver/receiver.ino
@@ -1,3 +1,4 @@
+#include "CaxixiConfig.h"
 
 #include <MIDI.h>
 #include <midi_Defs.h>
@@ -10,7 +11,7 @@ struct MyMidiSettings : public midi::DefaultSettings {
 };
 MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, MyMidiSettings);
 
-#include "CaxixiConfig.h"
+#include "CxCircularBuffer.h"
 #include <SoftwareSerial.h>
 SoftwareSerial XBee(2, 3); // Arduino RX, TX (XBee Dout, Din)
 
@@ -22,15 +23,96 @@ boolean started = false;
 boolean ended = false;
 int inInt;
 
+int cxRightForwardNote = CAXIXI_RIGHT_NOTE_FORWARD;
+int cxRightBackwardNote = CAXIXI_RIGHT_NOTE_BACKWARD;
+int cxRightHitNote = CAXIXI_RIGHT_NOTE_HIT;
+int cxLeftForwardNote = CAXIXI_LEFT_NOTE_FORWARD;
+int cxLeftBackwardNote = CAXIXI_LEFT_NOTE_BACKWARD;
+int cxLeftHitNote = CAXIXI_LEFT_NOTE_HIT;
+
+
+/////////////OCTAVADOR//////////
+int lastUpState = 0;
+int lastDownState = 0;
+int UpState = 0;	// variable for reading the pushbutton status
+int DownState = 0;
+int CurrentOctave = 0;
+//////////////////////////////////
+
+////////// SAMPLER ///////////////
+Buffer samples[SAMPLER_BUFFER_SIZE]; //puede almacenar hasta 50 notas (50 on, 50 off)
+int bufferI=0; //indice del buffer para record
+int bufferJ=0; //indice del buffer para play
+// BUTTON RECORD const & var:
+int buttonPushCounter = 0;				// counter for the number of button presses
+int buttonState = 0;							// current state of the button
+int lastButtonState = 0;					// previous state of the button
+int buttonNoteState = 0;
+int lastButtonNoteState = 0;
+int buttonUndoState = 0;
+int lastButtonUndoState = 0;
+
+int layer = 0;
+
+bool record = false;
+bool play = false;
+bool isSetT0 = false;
+bool isSetT1 = false;
+bool FirstNote = false;
+bool isSetOctavador = true;
+
+long time = 0;		//time = millis() cuenta los milisegundos desde q arranca
+long t0 = 0;			//cuando record==True, ajustamos una variable "t0" a esos msec y t = millis()-t0
+long t1 = 0;			//cuando record == False, ajustamos una variable "t1", t1 = millis-t0
+long reset = 0;		//y ajustamos la variable reset = millis - t0 - t1, que en principio va a ser cero, pero en cada vuelta va a ir incrementándose en el tamaño del loop
+///////////////////////////////////////////////
+
+
 void setup()
 {
+	pinMode(OCTAVE_LED_PIN, OUTPUT);// initialize the LED as an output:
+	pinMode(OCTAVE_UP_BUTTON_PIN, INPUT);// initialize the button pin as a input:
+	pinMode(OCTAVE_DOWN_BUTTON_PIN, INPUT);
+	pinMode(SAMPLER_BUTTON_RECORD_PIN, INPUT);
+	pinMode(SAMPLER_BUTTON_UNDO_PIN, INPUT);
+	pinMode(OCTAVE_UP_LED_RED_PIN, OUTPUT);
+	pinMode(OCTAVE_UP_LED_BLUE_PIN, OUTPUT);
+	pinMode(OCTAVE_UP_LED_GREEN_PIN, OUTPUT);
+	pinMode(OCTAVE_DOWN_LED_RED_PIN, OUTPUT);
+	pinMode(OCTAVE_DOWN_LED_BLUE_PIN, OUTPUT);
+	pinMode(OCTAVE_DOWN_LED_GREEN_PIN, OUTPUT);
+	analogWrite(OCTAVE_DOWN_LED_RED_PIN,255);
+	analogWrite(OCTAVE_DOWN_LED_GREEN_PIN, 255);
+	analogWrite(OCTAVE_UP_LED_RED_PIN, 255);
+	analogWrite(OCTAVE_UP_LED_GREEN_PIN, 255);
+	delay(500);
+	analogWrite(OCTAVE_UP_LED_RED_PIN,255);
+	analogWrite(OCTAVE_UP_LED_GREEN_PIN,0);
+	delay(500);
+	analogWrite(OCTAVE_UP_LED_GREEN_PIN,255);
+	analogWrite(OCTAVE_DOWN_LED_RED_PIN,0);
+	delay(500);
+	analogWrite(OCTAVE_DOWN_LED_RED_PIN,255);
+	analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,0);
+	delay(500);
+	analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,255);
 	MIDI.begin(1);
 	XBee.begin(9600);
 	Serial.begin(9600);
+	delay(100);
+	initSamplerBuffer();
+	delay(100);
 }
 
 void loop()
 {
+	time = millis()-(t0 + t1 + reset);
+	setReset();
+	PlayBuffer();
+	ButtonRecord();
+	ButtonUndo();
+	Lights();// turns on the LED every two button pushes by checking the modulo of the button push counter.
+	Octavador();
 	while(XBee.available() > 0){
 		char aChar = XBee.read();
 		if(aChar == '<'){
@@ -50,29 +132,29 @@ void loop()
 			// Use the value
 			//Serial.println(inInt);
 			if (inInt == CAXIXI_RIGHT_FORWARD_NOTEON){
-				MIDI.sendNoteOn(CAXIXI_RIGHT_NOTE_FORWARD,127,MIDI_CHANNEL);
+				SendNoteOn(cxRightForwardNote);
 			} else if (inInt == CAXIXI_RIGHT_FORWARD_NOTEOFF){
-				MIDI.sendNoteOff(CAXIXI_RIGHT_NOTE_FORWARD,127,MIDI_CHANNEL);
+				SendNoteOff(cxRightForwardNote);
 			} else if (inInt == CAXIXI_RIGHT_BACKWARD_NOTEON){
-				MIDI.sendNoteOn(CAXIXI_RIGHT_NOTE_BACKWARD,127,MIDI_CHANNEL);
+				SendNoteOn(cxRightBackwardNote);
 			} else if (inInt == CAXIXI_RIGHT_BACKWARD_NOTEOFF){	
-				MIDI.sendNoteOff(CAXIXI_RIGHT_NOTE_BACKWARD,127,MIDI_CHANNEL);
+				SendNoteOff(cxRightBackwardNote);
 			} else if (inInt == CAXIXI_RIGHT_HIT_NOTEON){	
-				MIDI.sendNoteOn(CAXIXI_RIGHT_NOTE_HIT,127,MIDI_CHANNEL);
+				SendNoteOn(cxRightHitNote);
 			} else if (inInt == CAXIXI_RIGHT_HIT_NOTEOFF){	
-				MIDI.sendNoteOff(CAXIXI_RIGHT_NOTE_HIT,127,MIDI_CHANNEL);
+				SendNoteOff(cxRightHitNote);
 			} else if (inInt == CAXIXI_LEFT_FORWARD_NOTEON){
-				MIDI.sendNoteOn(CAXIXI_LEFT_NOTE_FORWARD,127,MIDI_CHANNEL);
+				SendNoteOn(cxLeftForwardNote);
 			} else if (inInt == CAXIXI_LEFT_FORWARD_NOTEOFF){
-				MIDI.sendNoteOff(CAXIXI_LEFT_NOTE_FORWARD,127,MIDI_CHANNEL);
+				SendNoteOff(cxLeftForwardNote);
 			} else if (inInt == CAXIXI_LEFT_BACKWARD_NOTEON){	
-				MIDI.sendNoteOn(CAXIXI_LEFT_NOTE_BACKWARD,127,MIDI_CHANNEL);
+				SendNoteOn(cxLeftBackwardNote);
 			} else if (inInt == CAXIXI_LEFT_BACKWARD_NOTEOFF){	
-				MIDI.sendNoteOff(CAXIXI_LEFT_NOTE_BACKWARD,127,MIDI_CHANNEL);
+				SendNoteOff(cxLeftBackwardNote);
 			} else if (inInt == CAXIXI_LEFT_HIT_NOTEON){	
-				MIDI.sendNoteOn(CAXIXI_LEFT_NOTE_HIT,127,MIDI_CHANNEL);
+				SendNoteOn(cxLeftHitNote);
 			} else if (inInt == CAXIXI_LEFT_HIT_NOTEOFF){	
-				MIDI.sendNoteOff(CAXIXI_LEFT_NOTE_HIT,127,MIDI_CHANNEL);
+				SendNoteOff(cxLeftHitNote);
 			}
 			// Get ready for the next time
 			started = false;
@@ -84,83 +166,262 @@ void loop()
 	}
 }
 
-
-/*
-
-
-#include <MIDI.h>
-#include <midi_Defs.h>
-#include <midi_Message.h>
-#include <midi_Namespace.h>
-#include <midi_Settings.h>
-struct MyMidiSettings : public midi::DefaultSettings {
-	//static const bool UseRunningStatus = false; // Messes with my old equipment!
-	static const long DefaultSettings::BaudRate = 57600;
-};
-MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, MyMidiSettings);
-
-#include <SoftwareSerial.h>
-
-#include <CaxixiConfig.h>
-
-SoftwareSerial XBee(2, 3); // Arduino RX, TX (XBee Dout, Din)
-
-String serialMessage = "";
-boolean messageComplete = false;
-
-void setup()
-{
-	XBee.begin(57600);
-	serialMessage.reserve(10);
-}
-
-void loop()
-{
-	serialEvent();
-	if(messageComplete){
-		if(serialMessage == CAXIXI_RIGHT_FORWARD_NOTEON){
-			MIDI.sendNoteOn(CAXIXI_RIGHT_NOTE_FORWARD,127,MIDI_CHANNEL);
-		}else if(serialMessage == CAXIXI_RIGHT_FORWARD_NOTEOFF){
-			MIDI.sendNoteOff(CAXIXI_RIGHT_NOTE_FORWARD,127,MIDI_CHANNEL);
-		}else if(serialMessage == CAXIXI_RIGHT_BACKWARD_NOTEON){
-			MIDI.sendNoteOn(CAXIXI_RIGHT_NOTE_BACKWARD,127,MIDI_CHANNEL);
-		}else if(serialMessage == CAXIXI_RIGHT_BACKWARD_NOTEOFF){
-			MIDI.sendNoteOff(CAXIXI_RIGHT_NOTE_BACKWARD,127,MIDI_CHANNEL);
-		}else if(serialMessage == CAXIXI_RIGHT_HIT_NOTEON){
-			MIDI.sendNoteOn(CAXIXI_RIGHT_NOTE_HIT,127,MIDI_CHANNEL);
-		}else if(serialMessage == CAXIXI_RIGHT_HIT_NOTEOFF){
-			MIDI.sendNoteOff(CAXIXI_RIGHT_NOTE_HIT,127,MIDI_CHANNEL);
-		}else if(serialMessage == CAXIXI_LEFT_FORWARD_NOTEON){
-			MIDI.sendNoteOn(CAXIXI_LEFT_NOTE_FORWARD,127,MIDI_CHANNEL);
-		}else if(serialMessage == CAXIXI_LEFT_FORWARD_NOTEOFF){
-			MIDI.sendNoteOff(CAXIXI_LEFT_NOTE_FORWARD,127,MIDI_CHANNEL);
-		}else if(serialMessage == CAXIXI_LEFT_BACKWARD_NOTEON){
-			MIDI.sendNoteOn(CAXIXI_LEFT_NOTE_BACKWARD,127,MIDI_CHANNEL);
-		}else if(serialMessage == CAXIXI_LEFT_BACKWARD_NOTEOFF){
-			MIDI.sendNoteOff(CAXIXI_LEFT_NOTE_BACKWARD,127,MIDI_CHANNEL);
-		}else if(serialMessage == CAXIXI_LEFT_HIT_NOTEON){
-			MIDI.sendNoteOn(CAXIXI_LEFT_NOTE_HIT,127,MIDI_CHANNEL);
-		}else if(serialMessage == CAXIXI_LEFT_HIT_NOTEOFF){
-			MIDI.sendNoteOff(CAXIXI_LEFT_NOTE_HIT,127,MIDI_CHANNEL);
-		}
-		serialMessage = "";
-		messageComplete = false;
-	}
-}
-	
-void serialEvent() {
-	while (XBee.available()) {
-		char msg = (char)XBee.read();
-		if (msg == '\n') {
-			messageComplete = true;
-		}else{
-			serialMessage += msg; 
+void bubbleSort(Buffer a[], int bufferI) {//Optimizamos esto reemplazando size por i?
+	for(int k=0; k<=bufferI; k++) {
+		for(int o=0; o<(bufferI-(k+1)); o++) {
+			if(a[o].time > a[o+1].time) {
+				Buffer t = a[o];
+				a[o] = a[o+1];
+				a[o+1] = t;
+			}
 		}
 	}
 }
 
+void SendNoteOn(int note)
+{
+	if(record){
+		FirstNote = true;
+		setT0();
+		time = millis()-(t0 + t1 + reset);  //Esto es para que a la primera nota la guarde con time == 0
+		Buffer sample = {note, layer, 1, time};
+		samples[bufferI] = sample;
+	}
+	MIDI.sendNoteOn(note,127,MIDI_CHANNEL);
+	if(record){
+		bufferI++;  
+	}
+}
 
-*/
+void SendNoteOff(int note)
+{
+	if(record){
+		Buffer sample = {note, layer, 0, time};
+		samples[bufferI] = sample;
+	}
+	MIDI.sendNoteOff(note,127,MIDI_CHANNEL);
+	if(record){
+		bufferI++;  
+	}
+}
+
+void initSamplerBuffer(){//Esta funcion asigna time=9999999 a todos los valores del buffer
+	for(int k=0; k<SAMPLER_BUFFER_SIZE; k++){
+		samples[k] = Buffer_default;
+	}
+}
+
+void setReset(){
+	int a = t1 - time;
+	if(t0 != 0 && t1!=0 && a<=0){
+		bubbleSort(samples,bufferI);
+		reset = millis()-t0-t1;
+		bufferJ = 0;
+	}
+}
+
+void setT0()//A esta fc la llama ButtonRecord()
+{
+	if (record && !isSetT0 && FirstNote){
+		t0 = millis();
+	}
+	if (record && !isSetT0&& FirstNote){
+		isSetT0 = true;
+	}
+}
+
+void setT1(){
+	if(!record && !isSetT1 && t0 != 0){
+		t1 = millis() - t0;
+		isSetT1 = true;
+		play = true;
+	}
+}
+
+void PlayBuffer() {
+	if(play){
+		if(samples[bufferJ].time-time<0 && samples[bufferJ].time-time>-100){//Esto es un hardcodeo (el -100), corregirlo mirando el reloj en la V2, o dejarlo...
+			if(samples[bufferJ].encendido){
+				MIDI.sendNoteOn(samples[bufferJ].note,127,MIDI_CHANNEL);
+			}else{
+				MIDI.sendNoteOff(samples[bufferJ].note,127,MIDI_CHANNEL);
+			}
+			bufferJ++; //J se reinicia cuando da la vuelta, en la fc setReset 
+		}
+	}
+}
+
+void ButtonRecord() {
+	buttonState = digitalRead(SAMPLER_BUTTON_RECORD_PIN);
+	if (buttonState != lastButtonState) {// if the state has changed, increment the counter
+		if (buttonState == HIGH) {// if the current state is HIGH then the button wend from off to on:
+			buttonPushCounter++;
+			if (record==true){
+				record=false;
+				isSetOctavador=false;
+				setT1();
+			} else {
+				record=true;
+				layer = layer + 1;
+			}
+		}
+		lastButtonState = buttonState; // save the current state as the last state, for next time through the loop
+	}
+}
+
+void Undo(Buffer a[], int i, int layer){//Hay que usar la variable global i para que solamente recorra las notas que están "escritas". Lo mismo hay que hacer en el bubblesort
+                                       //Faltaría, además, que cuando son eliminadas algunas notas, se resten a i. Es decir, que i siempre de cuenta de la cantidad de notas en el buffer.
+                                       //Aunque eso implicaría llevar una cuenta de las notas que elimino.
+  for(int h=0; h<=bufferI; h++) {
+    if(a[h].layer == layer){
+      a[h] = Buffer_default;
+    }
+  }
+}
+
+void ButtonUndo()
+{
+  buttonUndoState = digitalRead(SAMPLER_BUTTON_UNDO_PIN);
+  if (buttonUndoState != lastButtonUndoState) {// if the state has changed, increment the counter
+    if (buttonUndoState == HIGH) {// if the current state is HIGH then the button wend from off to on:
+      if(layer>1){//Esto es para que nunca borre la capa original
+          Undo(samples,bufferI,layer);
+          if(layer>2){//Esto es para que no agregue notas a la capa original
+          layer = layer-1;
+          }
+      }
+    }
+  lastButtonUndoState = buttonUndoState; // save the current state as the last state, for next time through the loop
+  }
+}
+
+void Lights(){
+	// turns on the LED every two button pushes by checking the modulo of the button push counter.
+	if (buttonPushCounter % 2 == 1) {
+		digitalWrite(OCTAVE_LED_PIN, HIGH);
+	} else {
+		digitalWrite(OCTAVE_LED_PIN, LOW);
+	}
+}
+
+void Octavador(){
+	UpState = digitalRead(OCTAVE_UP_BUTTON_PIN);
+	DownState = digitalRead(OCTAVE_DOWN_BUTTON_PIN);
+	if (UpState != lastUpState) {
+		if (UpState == HIGH){
+			SendNoteOff(cxRightForwardNote);
+			SendNoteOff(cxRightBackwardNote);
+			SendNoteOff(cxRightHitNote);
+			SendNoteOff(cxLeftForwardNote);
+			SendNoteOff(cxLeftBackwardNote);
+			SendNoteOff(cxLeftHitNote);
+			
+			cxRightForwardNote = cxRightForwardNote+12;
+			cxRightBackwardNote = cxRightBackwardNote+12;
+			cxRightHitNote = cxRightHitNote+12;
+			cxLeftForwardNote = cxLeftForwardNote+12;
+			cxLeftBackwardNote = cxLeftBackwardNote+12;
+			cxLeftHitNote = cxLeftHitNote+12;
+			CurrentOctave = CurrentOctave+1; 
+			digitalWrite(OCTAVE_LED_PIN, HIGH);  
+		} else {
+			// turn LED off:
+			digitalWrite(OCTAVE_LED_PIN, LOW);
+		}
+		lastUpState = UpState;
+	}
+  if (DownState != lastDownState){     
+		if (DownState == HIGH) {    
+			SendNoteOff(cxRightForwardNote);
+			SendNoteOff(cxRightBackwardNote);
+			SendNoteOff(cxRightHitNote);
+			SendNoteOff(cxLeftForwardNote);
+			SendNoteOff(cxLeftBackwardNote);
+			SendNoteOff(cxLeftHitNote);
+			
+			cxRightForwardNote = cxRightForwardNote-12;
+			cxRightBackwardNote = cxRightBackwardNote-12;
+			cxRightHitNote = cxRightHitNote-12;
+			cxLeftForwardNote = cxLeftForwardNote-12;
+			cxLeftBackwardNote = cxLeftBackwardNote-12;
+			cxLeftHitNote = cxLeftHitNote-12;
+			CurrentOctave = CurrentOctave-1;
+			digitalWrite(OCTAVE_LED_PIN, HIGH);  
+		} else {
+			// turn LED off:
+			digitalWrite(OCTAVE_LED_PIN, LOW);
+		}
+		lastDownState = DownState;
+	}
+	showLeds();
+}
+
+void showLeds(){
+switch(CurrentOctave){
+    case 0: analogWrite(OCTAVE_UP_LED_RED_PIN,255);
+              analogWrite(OCTAVE_UP_LED_GREEN_PIN,255);
+              analogWrite(OCTAVE_DOWN_LED_RED_PIN,255);
+              analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,255);
+   
+    break;
+
+    case 1: analogWrite(OCTAVE_UP_LED_RED_PIN,0);
+              analogWrite(OCTAVE_UP_LED_GREEN_PIN,0);
+              analogWrite(OCTAVE_DOWN_LED_RED_PIN,255);
+              analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,255);
+   
+    break;
+
+    case 2: analogWrite(OCTAVE_UP_LED_RED_PIN,0);
+              analogWrite(OCTAVE_UP_LED_GREEN_PIN,75);
+              analogWrite(OCTAVE_DOWN_LED_RED_PIN,255);
+              analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,255);
+   
+    break;
+
+    case 3: analogWrite(OCTAVE_UP_LED_RED_PIN,0);
+              analogWrite(OCTAVE_UP_LED_GREEN_PIN,255);
+              analogWrite(OCTAVE_DOWN_LED_RED_PIN,255);
+              analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,255);
+   
+    break;
+
+    
+    case -1: analogWrite(OCTAVE_UP_LED_RED_PIN,255);
+              analogWrite(OCTAVE_UP_LED_GREEN_PIN,255);
+              analogWrite(OCTAVE_DOWN_LED_RED_PIN,0);
+              analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,0);
+   
+    break;
+
+    case -2: analogWrite(OCTAVE_UP_LED_RED_PIN,255);
+              analogWrite(OCTAVE_UP_LED_GREEN_PIN,255);
+              analogWrite(OCTAVE_DOWN_LED_RED_PIN,0);
+              analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,75);
+   
+    break;
+
+    case -3: analogWrite(OCTAVE_UP_LED_RED_PIN,255);
+              analogWrite(OCTAVE_UP_LED_GREEN_PIN,255);
+              analogWrite(OCTAVE_DOWN_LED_RED_PIN,0);
+              analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,255);
+   
+    break;
+}  
+}
+
+void TurnOffAll(){//Apaga todos los leds
+	analogWrite(OCTAVE_UP_LED_RED_PIN,255);
+	analogWrite(OCTAVE_UP_LED_GREEN_PIN,255);
+	analogWrite(OCTAVE_UP_LED_BLUE_PIN,255);
+	analogWrite(OCTAVE_DOWN_LED_RED_PIN,255);
+	analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,255);
+	analogWrite(OCTAVE_DOWN_LED_BLUE_PIN,255);
+}
+
+
+
+
 
 
 
